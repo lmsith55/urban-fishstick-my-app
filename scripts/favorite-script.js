7d2b61df02b7cd57416a74bd7d3972b92e57b720
lmsith55 <167916438+lmsith55@users.noreply.github.com>
2024-07-09 01:59:00 -0500

    Add files via upload

---
+++ scripts/favorite-script.js
@@ -0,0 +1,4 @@
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+  src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"
+ src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+ 
